fd025530d93a59169ed9c27b93693923dfc4617d
Enes <etas1409@gmail.com>
2024-02-28 17:15:54 +0300

    add span

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const messageDiv = document.querySelector("#messageDiv");
 validatebBtn.addEventListener("click", ()=>{
     const validateNumber = myInput.value;
     const p = document.createElement("p");
+    const span = document.createElement("span");
     const validateNumberArray = validateNumber.split("-");
     if(!isNaN(validateNumberArray[0]) &&
         !isNaN(validateNumberArray[1]) &&
@@ -12,7 +13,11 @@ validatebBtn.addEventListener("click", ()=>{
         validateNumberArray[0].length === 3 && 
         validateNumberArray[1].length === 2 &&
         validateNumberArray[2].length === 4 ){
-            p.textContent = `${validateNumber} is valid`; 
+            span.textContent = `${validateNumber}`;
+            span.style.color = "green"
+            messageDiv.appendChild(span);
+
+            p.textContent = ` is valid`; 
             messageDiv.appendChild(p);
             setTimeout(() => {
                 myInput.value = "";
@@ -20,7 +25,11 @@ validatebBtn.addEventListener("click", ()=>{
                 myInput.focus()
             }, 2000);
     }else{
-        p.textContent = `${validateNumber} is not valid`; 
+        span.textContent = `${validateNumber}`;
+        span.style.color = "red"
+        messageDiv.appendChild(span);
+
+        p.textContent = ` is not valid`; 
         messageDiv.appendChild(p);
         setTimeout(() => {
             myInput.value = "";
